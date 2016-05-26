1120488033d4ae4b40fdd7c077e5b03dd53a5b82
jlucka625 <jlucka@fiea.local>
2016-05-26 15:04:58 -0400

    Adding base project to github

---
+++ Source/RewindableReplayDemo/RewindableReplayDemoGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "RewindableReplayDemoGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARewindableReplayDemoGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ARewindableReplayDemoGameMode();
+};
+
+
+
